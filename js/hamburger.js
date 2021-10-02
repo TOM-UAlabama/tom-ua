8a4914cc99bcf390b33b30f5793b46b427cff956
Nathan Eads <eadsnf@gmail.com>
2021-10-01 21:31:17 -0500

    idk something

---
+++ js/hamburger.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("navOverlay").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("navOverlay").style.width = "0%";
+}
+
+function navHover() {
+  //document.getElementById('activePage').style.backgroundColor = '#900f09';
+  //document.getElementById('activePage').style.color = 'white';
+}
+
+function navReset() {
+  //document.getElementById('activePage').style.backgroundColor = 'white';
+  //document.getElementById('activePage').style.color = 'black';
+}
